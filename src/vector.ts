e15353542066fbc0d87d189511b7f9a9b00ca5a4
Pete <11158905+Pittawat2542@users.noreply.github.com>
2021-10-08 16:47:15 +0700

    feat: add vector type and operations

---
+++ src/vector.ts
@@ -0,0 +1,49 @@
+
+/**
+ * @param x integer represents x coordinate
+ * @param y integer represents y coordinate
+ * @returns vector with x, y coordinates
+ */
+export const getVector = (x = 0, y = 0) => ({ x, y }) as Vector;
+export type Vector = { x: number, y: number };
+
+/**
+ * @param vector represents position
+ * @returns size of the vector
+ */
+export const getVectorSize = (vector: Vector) => Math.sqrt(vector.x ** 2 + vector.y ** 2);
+
+/**
+ * @param vector1 represents first vector
+ * @param vector2 represents second vector. Default to vector at origin
+ * @returns distance between two vectors
+ */
+export const getVectorDistance = (vector1: Vector, vector2 = getVector()) => getVectorSize(getVector(vector1.x - vector2.x, vector1.y - vector2.y));
+
+/**
+ * @param vector represents vector
+ * @returns unit vector of the vector
+ */
+export const getUnitVector = (vector: Vector) => scalarMultiplyVector(1 / getVectorSize(vector), vector);
+
+/**
+ * @param vector1 represents first vector
+ * @param vector2 represents second vector. Default to vector at origin
+ * @returns new vector as a result of addition of two vectors
+ */
+export const addVector = (vector1: Vector, vector2 = getVector()) => getVector(vector1.x + vector2.x, vector1.y + vector2.y);
+
+/**
+ * @param vector1 represents first vector
+ * @param vector2 represents second vector. Default to vector at origin
+ * @returns new vector as a result of subtraction of two vectors
+ */
+export const subtractVector = (vector1: Vector, vector2 = getVector()) => getVector(vector1.x - vector2.x, vector1.y - vector2.y);
+
+/**
+ * 
+ * @param scalar represents scalar value
+ * @param vector represents vector
+ * @returns new vector as a result of multiplication of scalar and vector
+ */
+export const scalarMultiplyVector = (scalar: number, vector: Vector) => getVector(vector.x * scalar, vector.y * scalar);
